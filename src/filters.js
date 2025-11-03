893f8a0673fd13f046d15216b18da105932149be
Julian Cachau <j.cachau@vinson.com.ar>
2025-11-02 21:49:45 -0300

    Update filters.js

--- src/filters.js
+++ src/filters.js
@@ -1,3 +1,5 @@
+import { CONFIG } from './config.js';
+
 // src/filters.js
 function startOfDay(d){ const x=new Date(d); x.setHours(0,0,0,0); return x; }
 function endOfDay(d){ const x=new Date(d); x.setHours(23,59,59,999); return x; }
